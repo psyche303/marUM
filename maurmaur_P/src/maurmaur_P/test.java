106071644fd79413e6653f8ef4b3d014100d5168
psyche303 <37858319+psyche303@users.noreply.github.com>
2018-05-16 01:52:31 +0900

    selenium 구현 중_2 1. 첫 페이지 다운로드까지 완료 -> "다음 페이지"버튼 누르고 반복문까지 설정하기 -> 몇 번 누를 건지는, 처음에 만드는 arrayList 참조
    2. 코드가 너무 지저분한 것 같음..

--- maurmaur_P/src/maurmaur_P/test.java
+++ maurmaur_P/src/maurmaur_P/test.java
@@ -27,13 +27,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test implements Runnable {
 	private Object textLog;
-	String ThreadName;
+	String ThreadName, confirm, finImgSrc, cImgSrc;
 	List<String> comicList = new ArrayList<String>();
 	List<String> path = new ArrayList<String>();
 	List<String> namae = new ArrayList<String>();
 	JTextArea txtLog = new JTextArea();
-	String format, confirm;
+	String format = "";
 	File f;
+	List<String> imgSrc = new ArrayList<String>();
+	int endIndex;
 
 	test(List<String> comicList, List<String> path, List<String> namae, JTextArea txtLog, String ThreadName) {
 		this.comicList = comicList;
@@ -46,134 +48,165 @@ public class test implements Runnable {
 	@Override
 	public void run() {
 		long start = System.currentTimeMillis();
+
 		// for (int i = 0; i < comicList.size(); i++) {
-		try {// 한 권의 img를 저장하는 try
-			Document doc2 = Jsoup.connect(comicList.get(0)).get();
-			Element element2 = doc2.select("div.article-gallery").get(0);
-			Elements img = element2.select("img");
-			Elements pw = element2.select("input");
-			String sPw = pw.toString();
-			System.out.println(sPw);
-			System.out.println("sPw 출력 끝");
-			if (sPw.length() != 0) {
-				System.out.println("if문 안쪽");
-				System.out.println(pw);
-				System.out.println("if문 안쪽 실행");
-				try {
-					System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
-					System.out.println(System.getProperty("webdriver.chrome.driver"));
-					WebDriver driver = new ChromeDriver();
-					System.out.println("Chrome is selected");
-					driver.get(comicList.get(0));
-					Thread.sleep(300);
-					System.out.println(comicList.get(0) + "is selected");
-					WebElement sElement = driver.findElement(By.name("pass"));
-					WebElement sElement2 = driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[2]/div/form/input"));
-					System.out.println("\"pass\" is found");
-					sElement.click();
-					sElement.clear();
-					sElement.sendKeys("qndxkr");
-					sElement2.click();
-					System.out.println("submit까지 완료");
-					System.out.println("Page title is : " + driver.getTitle());
-					Thread.sleep(60000);
-					WebElement sElement3 = driver
-							.findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div[2]/div/span[1]"));
-					System.out.println(sElement3.getText());
-					
-					int fileNum = 1;
-					WebElement sElement10 = driver.findElement(By.className("lz-lazyloaded"));
-					
-					
-					
-					
-					
+		try {// 첫 권을 저장하는 try
+			System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+			System.out.println(System.getProperty("webdriver.chrome.driver"));
+			WebDriver driver = new ChromeDriver();
+			System.out.println("Chrome is selected");
+			driver.get(comicList.get(0));
+			System.out.println(comicList.get(0) + "is selected");
+			WebElement sElement = driver.findElement(By.id("gallery_vertical"));
+			System.out.println("\"gallery_vertical\" is found");
+			System.out.println("sElement : " + sElement.toString());
+			String inHtml = sElement.getAttribute("innerHTML");
+			System.out.println("-----------------------------------------------------------");
+			System.out.println("innerHTML: " + inHtml);
+			System.out.println("-----------------------------------------------------------");
 
-//					try {
-//						Document doc20 = Jsoup.connect(comicList.get(0)).get();
-//						Element element20 = doc20.select("div.article-gallery").get(0);
-//						Elements img0 = element20.select("img");
-//						int fileNum = 1;
-//						for (Element e2 : img0) {
-//
-//							String url3 = "http://wasabisyrup.com" + e2.getElementsByAttribute("src").attr("data-src");
-//							url3 = url3.replace(" copy", "%20copy");
-//							URL imgUrl = new URL(url3);
-//
-//							if (url3.contains(".jpg")) {
-//								format = ".jpg";
-//							} else if (url3.contains(".jpeg")) {
-//								format = ".jpeg";
-//							} else if (url3.contains(".gif")) {
-//								format = ".gif";
-//							} else if (url3.contains(".png")) {
-//								format = ".png";
-//							}
-//
-//							// 이미 있는 파일이라면 넘어감.
-//							confirm = path.get(0) + "\\" + namae.get(0) + "_" + fileNum + format;
-//							f = new File(confirm);
-//							if (f.exists()) {
-//								System.out.println(confirm + "은 이미 존재해서 넘어감");
-//							}
-//							// 이미지 다운로드
-//							else {
-//								HttpURLConnection conn = (HttpURLConnection) imgUrl.openConnection();
-//								conn.addRequestProperty("User-Agent", "Mozilla/4.76");
-//								System.out.println(conn.getContentLength());
-//
-//								InputStream is = conn.getInputStream();
-//								BufferedInputStream bis = new BufferedInputStream(is);
-//								FileOutputStream os = new FileOutputStream(
-//										path.get(0) + "\\" + namae.get(0) + "_" + fileNum + format);
-//
-//								BufferedOutputStream bos = new BufferedOutputStream(os);
-//								int byteImg;
-//
-//								// byte[] buf = new byte[conn.getContentLength()]; // 범위오류 때문에 1024byte단위로 읽게
-//								// 수정.
-//								byte[] buf = new byte[1024];
-//								while ((byteImg = bis.read(buf)) != -1) {
-//									bos.write(buf, 0, byteImg);
-//								}
-//								txtLog.append(namae.get(0) + "_" + fileNum + " 저장완료" + "  src : " + imgUrl + "\n");
-//								txtLog.setCaretPosition(txtLog.getDocument().getLength());
-//								fileNum += 1;
-//								bos.close();
-//								os.close();
-//								bis.close();
-//								is.close();
-//							}
-//
-//						}
-//
-//					} catch (IOException e2) {
-//						System.out.println(e2.getMessage());
-//						txtLog.append(e2.getMessage() + "\n");
-//					}
+			String splitHTML[] = inHtml.split("src=");
 
-					// } 처음 for문의 괄호
-
-					long end = System.currentTimeMillis();
-					txtLog.append(ThreadName + " is Done ____ " + "실행 시간 : " + (end - start) / 1000.0 + "초\n");
-					txtLog.setCaretPosition(txtLog.getDocument().getLength());
-
-					System.out.println("해보자");
-					WebElement sElement4 = driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div[1]/div[2]/a"));
-					System.out.println("버튼찾음");
-					System.out.println(sElement4.getText());
-					sElement4.click();
-					System.out.println("클릭");
-
-				} catch (WebDriverException | InterruptedException sE) {
-					System.out.println("에러발생");
+			System.out.println("아래에 sp 표시\n");
+			for (int i = 0; i < splitHTML.length; i++) {
+				cImgSrc = "http://wasabisyrup.com" + splitHTML[i].substring(1, splitHTML[i].length());
+				System.out.println("cImgSrc : " + cImgSrc);
+				if (cImgSrc.contains(".jpg")) {
+					format = ".jpg";
+				} else if (cImgSrc.contains(".jpeg")) {
+					format = ".jpeg";
+				} else if (cImgSrc.contains(".png")) {
+					format = ".png";
+				} else if (cImgSrc.contains(".bmp")) {
+					format = ".bmp";
+				} else if (cImgSrc.contains(".png")) {
+					format = ".png";
 				}
-				System.out.println("if문 안쪽 끝");
+				if (cImgSrc.contains(format)) {
+					endIndex = cImgSrc.indexOf(format);
+					finImgSrc = cImgSrc.substring(0, endIndex) + format;
+					imgSrc.add(finImgSrc);
+				}
 			}
 
-		} catch (Exception e33) {
-			System.out.println("마기막 에러요");
+			for (int i = 0; i < imgSrc.size() - 1; i++) {
+				for (int j = 1; j < imgSrc.size(); j++) {
+					if (imgSrc.get(i).equals(imgSrc.get(j))) {
+						imgSrc.remove(j);
+					} else if (imgSrc.get(i).equals("")) {
+						imgSrc.remove(i);
+					} else if (imgSrc.get(j).equals("")) {
+						imgSrc.remove(j);
+					}
+				}
+			}
+			System.out.println("All process is Done");
+			System.out.println(imgSrc);
 
+			driver.quit();
+			System.out.println("driver 종료");
+
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
+
+		try {
+
+			int fileNum = 1;
+			for (int i = 0; i < imgSrc.size(); i++) {
+				confirm = path.get(0) + "\\" + namae.get(0) + "_" + fileNum + format;
+				f = new File(confirm);
+				if (f.exists()) {
+					System.out.println(confirm + "은 이미 존재해서 넘어감");
+				}
+
+				URL a2 = new URL(imgSrc.get(i));
+				System.out.println("imgSrc.get("+i+") : "+imgSrc.get(i) + "작업");
+				HttpURLConnection conn = (HttpURLConnection) a2.openConnection();
+				conn.addRequestProperty("User-Agent", "Mozilla/4.76");
+
+				System.out.println(conn.getContentLength());
+
+				InputStream is = conn.getInputStream();
+				BufferedInputStream bis = new BufferedInputStream(is);
+				FileOutputStream os = new FileOutputStream(path.get(0) + "\\" + namae.get(0) + "_" + fileNum + format);
+
+				BufferedOutputStream bos = new BufferedOutputStream(os);
+				int byteImg;
+
+				byte[] buf = new byte[1024];
+				while ((byteImg = bis.read(buf)) != -1) {
+					bos.write(buf, 0, byteImg);
+				}
+				bos.close();
+				os.close();
+				bis.close();
+				is.close();
+				fileNum++;
+			}
+
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+
+		////////////////// 첫 권 저장 끝
+
+		// Document doc2 = Jsoup.connect(comicList.get(0)).get();
+		// Element element2 = doc2.select("div.article-gallery").get(0);
+		// Elements img = element2.select("img");
+		// Elements pw = element2.select("input");
+		// String sPw = pw.toString();
+		// System.out.println(sPw);
+		// System.out.println("sPw 출력 끝");
+		// if (sPw.length() != 0) {
+		// System.out.println("if문 안쪽");
+		// System.out.println(pw);
+		// System.out.println("if문 안쪽 실행");
+		// try {
+		// System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+		// System.out.println(System.getProperty("webdriver.chrome.driver"));
+		// WebDriver driver = new ChromeDriver();
+		// System.out.println("Chrome is selected");
+		// driver.get(comicList.get(0));
+		// Thread.sleep(300);
+		// System.out.println(comicList.get(0) + "is selected");
+		// WebElement sElement = driver.findElement(By.name("pass"));
+		// WebElement sElement2 =
+		// driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[2]/div/form/input"));
+		// System.out.println("\"pass\" is found");
+		// sElement.click();
+		// sElement.clear();
+		// sElement.sendKeys("qndxkr");
+		// sElement2.click();
+		// System.out.println("submit까지 완료");
+		// System.out.println("Page title is : " + driver.getTitle());
+		// Thread.sleep(60000);
+		// WebElement sElement3 = driver
+		// .findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div[2]/div/span[1]"));
+		// System.out.println(sElement3.getText());
+		//
+		// int fileNum = 1;
+		// WebElement sElement10 = driver.findElement(By.className("lz-lazyloaded"));
+
+		long end = System.currentTimeMillis();
+		txtLog.append(ThreadName + " is Done ____ " + "실행 시간 : " + (end - start) / 1000.0 + "초\n");
+		txtLog.setCaretPosition(txtLog.getDocument().getLength());
+
+		// System.out.println("해보자");
+		// WebElement sElement4 =
+		// driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div[1]/div[2]/a"));
+		// System.out.println("버튼찾음");
+		// System.out.println(sElement4.getText());
+		// sElement4.click();
+		// System.out.println("클릭");
+		// } catch (WebDriverException | InterruptedException sE) {
+		// System.out.println("에러발생");
+		// }
+		// System.out.println("if문 안쪽 끝");
+		// }
+		//
+		// } catch (Exception e33) {
+		// System.out.println("마기막 에러요");
+
 	}
 }
--- maurmaur_P/src/maurmaur_P/test2.java
+++ maurmaur_P/src/maurmaur_P/test2.java
@@ -28,74 +28,100 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class test2 {
 
 	public static void main(String[] args) {
-		// try {
 
 		System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+
+		File f = new File(".\\gg.txt");
+
+		if (f.exists()) {
+			System.out.println("File existed");
+		} else {
+			System.out.println("File not found!");
+		}
+
 		System.out.println(System.getProperty("webdriver.chrome.driver"));
 		WebDriver driver = new ChromeDriver();
 		System.out.println("Chrome is selected");
-		driver.get("https://movie.naver.com/movie/bi/mi/basic.nhn?code=155123");
-		System.out.println("https://movie.naver.com/" + "is selected");
-		WebElement sElement = driver.findElement(By.className("photo_video"));
-		System.out.println("\"photo_video\" is found");
+		driver.get("http://wasabisyrup.com/archives/W70aImx_IwE");
+		System.out.println("http://wasabisyrup.com/archives/W70aImx_IwE" + "is selected");
+		WebElement sElement = driver.findElement(By.id("gallery_vertical"));
+		System.out.println("\"gallery_vertical\" is found");
 		System.out.println("sElement : " + sElement.toString());
 		String a = sElement.getAttribute("innerHTML");
-		System.out.println("a: "+ a);
+		System.out.println("a: " + a);
 
 		String sp[] = a.split("src=");
 		List<String> spp = new ArrayList<String>();
 		int st, ed;
-		String a1;
-		
-		
+		String a1, h;
+		String format = "";
 
+		System.out.println("아래에 sp 표시\n");
 		for (int i = 0; i < sp.length; i++) {
-//			System.out.println("sp["+i+"] : "+sp[i]);
-			if (sp[i].contains(".jpg")) {
-				st = sp[i].indexOf("https://");
-				ed = sp[i].indexOf(".jpg");
-				a1 = sp[i].substring(st,ed+4);
-				System.out.println(a1);
+			h = "http://wasabisyrup.com" + sp[i].substring(1, sp[i].length());
+			// System.out.println("sp[" + i + "] : " + sp[i]);
+			System.out.println("h : " + h);
+			if (h.contains(".jpg")) {
+				format = ".jpg";
+			} else if (h.contains(".jpeg")) {
+				format = ".jpeg";
+			} else if (h.contains(".png")) {
+				format = ".png";
+			} else if (h.contains(".bmp")) {
+				format = ".bmp";
+			} else if (h.contains(".png")) {
+				format = ".png";
+			}
+			if (h.contains(format)) {
+				ed = h.indexOf(format);
+				a1 = h.substring(0, ed) + format;
 				spp.add(a1);
 			}
-			
+		}
+
+		for (int i = 0; i < spp.size() - 1; i++) {
+			for (int j = 1; j < spp.size(); j++) {
+				if (spp.get(i).equals(spp.get(j))) {
+					spp.remove(j);
+				} else if (spp.get(i).equals("")) {
+					spp.remove(i);
+				} else if (spp.get(j).equals("")) {
+					spp.remove(j);
+				}
+			}
 		}
 		System.out.println("All process is Done");
 		System.out.println(spp);
 
-		// int st = a.indexOf("https://");
-		// int ed = a.indexOf(".jpg");
-		//
-		// String a1 = a.substring(st,ed+4);
-		// System.out.println(a1);
-		//
-		//
-		//
-		// URL a2 = new URL(a1);
-		// HttpURLConnection conn = (HttpURLConnection) a2.openConnection();
-		// conn.addRequestProperty("User-Agent", "Mozilla/4.76");
-		// System.out.println(conn.getContentLength());
-		//
-		// InputStream is = conn.getInputStream();
-		// BufferedInputStream bis = new BufferedInputStream(is);
-		// FileOutputStream os = new FileOutputStream(
-		// "C:\\Users\\2-10\\Desktop\\크롤링테스트\\붕탁테스트_" + "1" + ".jpg");
-		//
-		// BufferedOutputStream bos = new BufferedOutputStream(os);
-		// int byteImg;
-		//
-		// // byte[] buf = new byte[conn.getContentLength()]; // 범위오류 때문에 1024byte단위로 읽게
-		// // 수정.
-		// byte[] buf = new byte[1024];
-		// while ((byteImg = bis.read(buf)) != -1) {
-		// bos.write(buf, 0, byteImg);
-		// }
-		// bos.close();
-		// os.close();
-		// bis.close();
-		// is.close();
-		// }catch(Exception e1) {
-		// System.out.println("오류");
-		// }
+		driver.quit();
+		System.out.println("driver 종료");
+
+		try {
+			for (int i = 0; i < spp.size(); i++) {
+				URL a2 = new URL(spp.get(i));
+
+				HttpURLConnection conn = (HttpURLConnection) a2.openConnection();
+				conn.addRequestProperty("User-Agent", "Mozilla/4.76");
+				System.out.println(conn.getContentLength());
+
+				InputStream is = conn.getInputStream();
+				BufferedInputStream bis = new BufferedInputStream(is);
+				FileOutputStream os = new FileOutputStream("E:\\크롤링 연습\\붕탁 테스트_" + i + ".jpg");
+
+				BufferedOutputStream bos = new BufferedOutputStream(os);
+				int byteImg;
+
+				byte[] buf = new byte[1024];
+				while ((byteImg = bis.read(buf)) != -1) {
+					bos.write(buf, 0, byteImg);
+				}
+				bos.close();
+				os.close();
+				bis.close();
+				is.close();
+			}
+		} catch (Exception e1) {
+			System.out.println("오류");
+		}
 	}
 }
